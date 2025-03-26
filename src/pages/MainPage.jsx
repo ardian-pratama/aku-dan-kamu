a318e0fbe46cc887a205b3e9b702c42efa9ba02d
Ardian Pratama <ardnpratm@gmail.com>
2025-03-26 21:20:38 +0700

    fix(audio) perbaikan pada audio

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,51 @@
 import Lyric from '../components/Lyric.jsx';
 import Image from '../components/Image.jsx';
 import image from '../assets/image/IMG-20250303-WA0042.jpg';
-import AudioPlayer from '../components/AudioPlayer.jsx'
+import { useEffect, useRef, useState } from 'react';
+import { Howl } from 'howler';
+import music from '../assets/audio/h.mp3';
 
 export default function MainPage() {
+  const soundRef = useRef(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    soundRef.current = new Howl({
+      src: music,
+      preload: true,
+      volume: 0,
+      loop: true,
+      onload: () => {
+        setloading(false);
+        soundRef.current?.play();
+        soundRef.current?.volume(1.0);
+      },
+    });
+
+    return () => {
+      soundRef.current?.stop();
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col gap-5'>
-      <Lyric></Lyric>
-      <Lyric></Lyric>
-      <Image
-        src={image}
-        description='Lorem ipsum dolor sit amet'
-      />
-      <Lyric></Lyric>
-      <Image
-        src={image}
-        description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora consequuntur, in. Excepturi, autem suscipit nam!'
-      />
-      <AudioPlayer></AudioPlayer>
-    </div>
+    <>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <div className='flex flex-col gap-5'>
+          <Lyric></Lyric>
+          <Lyric></Lyric>
+          <Image
+            src={image}
+            description='Lorem ipsum dolor sit amet'
+          />
+          <Lyric></Lyric>
+          <Image
+            src={image}
+            description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora consequuntur, in. Excepturi, autem suscipit nam!'
+          />
+        </div>
+      )}
+    </>
   );
 }
