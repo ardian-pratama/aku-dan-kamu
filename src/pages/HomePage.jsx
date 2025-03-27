19e9befe17b86d2b287fc80ad2922df17ec13dbd
Ardian Pratama <ardnpratm@gmail.com>
2025-03-27 07:37:12 +0700

    feat(ui) menambahkan komponen avatar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,7 @@ import {
 import { motion } from 'motion/react';
 import Section from '../components/Section.jsx';
 import Timer from '../components/Timer.jsx';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const cards = [
   {
@@ -34,9 +35,7 @@ export default function HomePage() {
         <div className='md:col-span-3 flex flex-col gap-5'>
           <div className='flex flex-col self-center font-agbalumo text-5xl text-primary relative'>
             <h1>Aku</h1>
-            <h1 className='absolute left-[86px] top-7 text-blue-500'>
-              &amp;
-            </h1>
+            <h1 className='absolute left-[86px] top-7 text-blue-500'>&amp;</h1>
             <h1 className='ml-[120px]'>Kamu</h1>
           </div>
           <p className='text-justify'>
@@ -46,12 +45,14 @@ export default function HomePage() {
           </p>
           <Timer />
         </div>
-        <div className='md:col-span-2'>
-          <img
+        <Avatar className='md:col-span-2 w-full h-auto'>
+          <AvatarImage
             src='/aku-dan-kamu.png'
-            alt=''
+            alt='Aku dan Kamu'
+            className='rounded-none'
           />
-        </div>
+          <AvatarFallback/>
+        </Avatar>
       </Section>
       <Section title='Tentang Kita'>
         <div className='grid md:grid-cols-2 gap-5'>
